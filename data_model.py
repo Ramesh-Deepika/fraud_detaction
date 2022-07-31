2632a359bf50d8fd3896f5e6e15e51fdd50c6187
Ramesh-Deepika <deepi26.mys@gmail.com>
2022-07-31 14:00:35 +0530

    added model functions

--- data_model.py
+++ data_model.py
@@ -1,1 +1,9 @@
 print("this function holds the functionality of data modelling")
+
+ def model1():
+    print("adding the code for ML model1")
+
+def model2():
+    print("adding the code for ML model2")
+ def model3():
+    print("adding the code for ML model3")
