6b3e4d838650fcf29b0492911585f24cffdad73c
Ramesh-Deepika <deepi26.mys@gmail.com>
2022-07-31 13:48:50 +0530

    adding the different models

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,9 @@
  print("adding all the data processing codes related to fraud detection")
+
+ def model1():
+    print("adding the code for ML model1")
+
+def model2():
+    print("adding the code for ML model2")
+ def model3():
+    print("adding the code for ML model3")
